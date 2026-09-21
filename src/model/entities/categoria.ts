7ff8b3d65447ab77c97f1e6ccb4b07ef01f0a7f8
Heber-Bringel <heberbringel123@gmail.com>
2026-09-21 14:04:56 -0300

    refactor: implementa entidades

---
+++ src/model/entities/categoria.ts
@@ -0,0 +1,7 @@
+export type categoria = {
+    id: string;
+    nome: string;
+    corBorda: string;
+    corSeta: string;
+    imagemUrl: string;
+}
---
+++ src/model/entities/produto.ts
@@ -0,0 +1,11 @@
+export type produto = {
+    id: string;
+    categoriaid: string;
+    categoriaNome: string;
+    preco: number;
+    descricao: string;
+    proteinas: number;
+    carboidratos: number;
+    imagemPequenaUrl: string;
+    imagemGrandeUrl: string;
+}
